b9ae0806a772d109acd52169ccd76f425f7faf8d
zchdvs <zchdvs232@gmail.com>
2022-10-13 13:47:15 -0700

    Adding Web Gui

---
+++ WebGui/src/routes.js
@@ -0,0 +1,96 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import Icons from "views/Icons.js";
+import Map from "views/Map.js";
+import Notifications from "views/Notifications.js";
+import Rtl from "views/Rtl.js";
+import TableList from "views/TableList.js";
+import Typography from "views/Typography.js";
+import UserProfile from "views/UserProfile.js";
+
+var routes = [
+  {
+    path: "/COMTRADE",
+    name: "COMTRADE File",
+    rtlName: "لوحة القيادة",
+    icon: "fa-solid fa-file-waveform",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/SignalCreation",
+    name: "Signal Creation",
+    rtlName: "الرموز",
+    icon: "fa-solid fa-wave-square",
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/Wiki",
+    name: "Documentation",
+    rtlName: "خرائط",
+    icon: "fa-solid fa-book-atlas",
+    component: Notifications,
+    layout: "/admin"
+  }
+];
+var oldRoutes = [
+  ,
+  {
+    path: "/notifications",
+    name: "Notifications",
+    rtlName: "إخطارات",
+    icon: "tim-icons icon-bell-55",
+    component: Notifications,
+    layout: "/admin"
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: "tim-icons icon-single-02",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/tables",
+    name: "Table List",
+    rtlName: "قائمة الجدول",
+    icon: "tim-icons icon-puzzle-10",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    rtlName: "طباعة",
+    icon: "tim-icons icon-align-center",
+    component: Typography,
+    layout: "/admin"
+  },
+  {
+    path: "/rtl-support",
+    name: "RTL Support",
+    rtlName: "ار تي ال",
+    icon: "tim-icons icon-world",
+    component: Rtl,
+    layout: "/rtl"
+  }
+];
+export default routes;
